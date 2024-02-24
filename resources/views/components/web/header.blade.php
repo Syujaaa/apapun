089cfc759b0d7cac8a35f02e9ef44a5ce4c1b91e
Syujaa <syujafarras@gmail.com>
2024-02-24 12:12:40 +0700

    first commit

---
+++ resources/views/components/web/header.blade.php
@@ -0,0 +1,14 @@
+<nav class="navbar bg-body-tertiary">
+  <div class="container">
+    <a class="navbar-brand">
+        <img src="{{ asset('assets/image/App-logo.png')}}" alt="" srcset="">
+    </a>
+    <!-- <form class="d-flex" role="search">
+      <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
+      <button class="btn btn-outline-success" type="submit">Search</button>
+    </form> -->
+    <div class="d-flex">
+        <button class="btn btn-white">Download App</button><button class="btn-warning btn">Login</button>
+    </div>
+  </div>
+</nav>
---
+++ resources/views/web/home/index.blade.php
@@ -0,0 +1,5 @@
+<x-web.layout>
+
+<img src="{{ asset('assets/image/banner.png') }}" alt="" srcset="">
+
+</x-web.layout>
